fdbd5d5948e9006cd3196f3311cb382f1b751151
John Necir Rebellion <johnnecirrebellion@gmail.com>
2023-12-19 17:40:47 +0800

    Fix

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 )
 
@@ -20,10 +21,33 @@ func main() {
 	}
 
 	app := fiber.New()
+	app.Use(logger.New())
 
-	app.Post("/api/webhook/staging", func(c *fiber.Ctx) error {
+	app.Post("payment_requests", func(c *fiber.Ctx) error {
+		log.Printf("Caller IP: %s\n", c.IPs())
 		requestBody := bytes.NewBuffer(c.Body())
-		resp, err := http.Post(fmt.Sprintf("%s/api/webhook", os.Getenv("DOMAIN")), "application/json", requestBody)
+		client := &http.Client{}
+		req, _ := http.NewRequest("POST", fmt.Sprintf("%s/payment_requests", os.Getenv("DOMAIN")), requestBody)
+		req.Header.Set("Content-Type", "application/json")
+		req.SetBasicAuth(os.Getenv("XENDIT_SECRET_API_KEY"), "")
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+
+		log.Println(string(body))
+
+		return c.Send(body)
+	})
+
+	app.Post("/payment_requests", func(c *fiber.Ctx) error {
+		log.Printf("Caller IP: %s\n", c.IPs())
+		requestBody := bytes.NewBuffer(c.Body())
+		resp, err := http.Post(fmt.Sprintf("%s/payment_requests", os.Getenv("DOMAIN")), "application/json", requestBody)
 
 		if err != nil {
 			log.Fatalln(err)
